701c5288a449062fa7b8b468efbf9a032549e995
Kevin Cruz <kevin_cruzz@outlook.es>
2023-04-21 17:29:56 -0600

    Added animations on abilities section and added form component

--- src/Components/Abilities/index.js
+++ src/Components/Abilities/index.js
@@ -4,10 +4,10 @@ import './abilities.css';
 export const Abilities = () => {
     return (
         <div className='abilities'>
-            <AbilityCard direction='left-direction'/>
-            <AbilityCard direction='right-direction'/>
-            <AbilityCard direction='left-direction'/>
-            <AbilityCard direction='right-direction'/>
+            <AbilityCard direction='left-direction' level={20} visibleLevel={2000} measureBar='first-bar' moveBarTo='right'/>
+            <AbilityCard direction='right-direction' level={60} visibleLevel={2200} measureBar='second-bar' moveBarTo='left'/>
+            <AbilityCard direction='left-direction' level={55} visibleLevel={2400} measureBar='third-bar' moveBarTo='right'/>
+            <AbilityCard direction='right-direction' level={30} visibleLevel={2600} measureBar='fourth-bar' moveBarTo='left'/>
         </div>
     );
 }
--- src/Components/AbilityCard/index.js
+++ src/Components/AbilityCard/index.js
@@ -2,23 +2,32 @@ import { useState } from 'react';
 import './abilityCard.css';
 
 export const AbilityCard = (props) => {
-    const [yPosition, displayBar] = useState(0);
 
     document.addEventListener('scroll', (e) => {
-        console.log(window.scrollY)
-        if (window.scrollY >= 2300) {
-            displayBar(() => {
-                const bar = document.querySelector('.measure');
-                bar.style.cssText = 'background-position: 10%;';
-            });
+
+        let bar = document.querySelector('.' + props.measureBar);
+
+        const barAnimation = (bar, props) => {
+            let bounceBasis = (props.direction === 'left-direction') ? -7 : 7;
+            bar.style.cssText = 'background-position: ' + (props.level + bounceBasis) + '%;';
+            setTimeout(() => {
+                bar.style.cssText = 'background-position: ' + props.level + '%;';
+            }, 600);
         }
+
+        if (window.scrollY >= props.visibleLevel && bar.style.cssText !== 'background-position: ' + props.level + '% center;') {
+            barAnimation(bar, props);
+        } else if (window.scrollY < props.visibleLevel) {
+            //remove animation
+            bar.style.cssText = 'background-position: ' + props.moveBarTo + ';';
+        } 
     });
 
     return (
         <div className='abilityCard'>
             <div className={props.direction}>
                 <div className='branding'></div>
-                <div className='measure'></div>
+                <div className={props.measureBar + ' measure'}></div>
             </div>
         </div>
     );
